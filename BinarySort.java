efb6997b44cb5d3e78b5b14650a207621e84951a
saku <saku3ds@gmail.com>
2016-05-16 00:48:43 +0800

    BinarySort class
    create BinarySort class 
    ※not complete

---
+++ BinarySort.java
@@ -0,0 +1,17 @@
+
+// U10111003 朱永捷
+
+public class BinarySort{
+
+	// data field to define Datum point
+	private Node datum;
+
+	// No-arugument constructor
+	public BinarySort() {
+	}
+
+	// Constructor
+	public BinarySort(int value) {
+		datum = new Node(value);
+	}
+}
