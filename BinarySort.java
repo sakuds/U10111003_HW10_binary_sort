14e2ada963888d95745cc8e8bc6043269f2ec3c5
saku <saku3ds@gmail.com>
2016-05-16 14:19:43 +0800

    add insert method and print method
    use while root to compare and create binary tree
    ※add set method which can use easily

--- BinarySort.java
+++ BinarySort.java
@@ -3,7 +3,7 @@
 
 public class BinarySort{
 
-	// data field to define Datum point
+	// Data field to define Datum point
 	private Node datum;
 
 	// No-arugument constructor
@@ -13,5 +13,47 @@ public class BinarySort{
 	// Constructor
 	public BinarySort(int value) {
 		datum = new Node(value);
+	}
+
+	// Set method
+	public void setDatum(int value) {
+		datum = new Node(value);
+	}
+
+	// Get method
+	public Node getDatum() {
+		return datum;
+	}
+
+	// Compare to insert right or left
+	public void insert(int data){
+		int time = 0;
+		Node com = datum;
+		while (time == 0){
+			if (data > com.getData()) {
+				if (com.right == null){
+					com.right = new Node(data);
+					time = 1;
+				} else {
+					com = com.right;
+				}
+			} else {
+				if (com.left == null){
+					com.left = new Node(data);
+					time = 1;
+				} else {
+					com = com.left;
+				}
+			}
+		}
+	}
+
+	// To print result
+	public void print(Node node) {
+		if (node != null) {
+			print(node.left);
+			System.out.print( node.getData() + "  ");
+			print(node.right);
+  		}
 	}
 }
